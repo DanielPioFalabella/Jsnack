40bc3ef0226e824f791c7bda275d3921909c417d
Daniel Pio Falabella <danielpiofalabella001@gmail.com>
2025-12-04 11:34:46 +0100

    miglioramento

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 // creo 2 numeri casuali
-const num1 = Math.floor(Math.random() * 100);
-const num2 = Math.floor(Math.random() * 100);
+const num1 = Number(prompt("inserisci un numero"));
+const num2 = Number(prompt("inserisci un numero"));
 let numero
 
 // definisco qual è il maggiore
 if (num1 > num2) {
     numero = num1
-    console.log(num1)
 } else {
     numero = num2
-    console.log(num2)
 }
 
 console.log(numero);
