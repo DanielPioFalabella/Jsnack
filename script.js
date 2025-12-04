2f0b96476ec573d3885beb0caf9f278d677ac1dc
Daniel Pio Falabella <danielpiofalabella001@gmail.com>
2025-12-04 12:44:54 +0100

    esercizio 2

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// esercizio 1
 // creo 2 numeri casuali
 const num1 = Number(prompt("inserisci un numero"));
 const num2 = Number(prompt("inserisci un numero"));
@@ -16,4 +17,15 @@ if (num1 > num2) {
 
 console.log("il numero maggiore è", numero);
 
+// esercizio 2
+// do la condizione in un ciclo for
+let sommaNumeri = 0;
+
+for (let i = 0; i < 5; i++) {
+    const numeriUtente = parseInt(prompt("inserisci un numero"));
+    sommaNumeri += numeriUtente;
+    console.log(numeriUtente);
+}
+
+console.log("La somma dei numeri scelti dall'Utente è:" + sommaNumeri);
 
